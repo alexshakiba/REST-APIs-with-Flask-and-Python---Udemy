67f5b8c92c965a27450fbbcf1b3acd63c20d9313
Alex Shakiba <alexshakiba@gmail.com>
2018-05-04 10:37:11 -0400

    fixed not importing db when running the app

---
+++ run.py
@@ -0,0 +1,10 @@
+from app import init_app
+from db import db
+
+#this file needs to be created because app.py will only work when __name__ __main__
+#when we run from heroku without this .py, it will give us an error
+db.init_app(app)
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
